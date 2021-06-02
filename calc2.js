da89b04e176e4caab868af14ef75c4c92519438a
nick <nick51365@gmail.com>
2021-06-02 19:41:51 -0400

    fixed glitch with = key

--- calc2.js
+++ calc2.js
@@ -1,6 +1,5 @@
 let displayValue = "";
-let saveValue = " ";
-let runningTotal = "";
+let saveValue = "";
 let operation = "";
 let btnValue = "";
 let newNumber = true;
@@ -8,6 +7,7 @@ let newNumber = true;
 const display = document.getElementById("display");
 const clear = document.getElementById("clear");
 const numButtons = document.querySelectorAll("button.numBtn");
+const btnEquals = document.getElementById("btnEquals");
 
 //Adds click listeners to number buttons
 for (i = 0; i < numButtons.length; i++){
@@ -21,6 +21,13 @@ document.getElementById("btnSubtract").addEventListener("click",() => setOperati
 document.getElementById("btnMultiply").addEventListener("click",() => setOperation("Multiply"));
 document.getElementById("btnDivide").addEventListener("click",() => setOperation("Divide"));
 
+//Adds click listener to equals button
+btnEquals.addEventListener("click",() => operate());
+btnEquals.addEventListener("click",() => saveValue = "");
+
+//Adds click listener to clear button
+clear.addEventListener("click",() => clearDisplay());
+
 function addDisplay(btnValue){  
     if (newNumber == true){
         displayValue = "";
@@ -29,10 +36,29 @@ function addDisplay(btnValue){
     newNumber = false;
     displayValue += btnValue;
     display.textContent = displayValue;
+    console.log("displayValue",displayValue);
+    console.log("saveValue",saveValue);
+}
+
+function clearDisplay(){
+    saveValue = "";
+    displayValue = "";
+    display.textContent = displayValue;
 }
 
 //Sets "operation" variable when operation button is clicked
 function setOperation(a){
+    //Moves displayValue to saveValue if blank. Otherwise, use displayValue to operate
+    if (saveValue == ""){
+        saveValue = displayValue;
+    
+        console.log("overwrite blank",saveValue);
+        console.log(operation);
+    
+        }else{
+            operate();
+        }    
+    
     newNumber = true;
     if (a == "Add"){
         operation = "Add"
@@ -44,24 +70,46 @@ function setOperation(a){
         operation = "Divide"
     }
 
-    //Moves displayValue to saveValue
-    if (saveValue = " "){
-    saveValue = displayValue;
-    console.log("overwrite blank",saveValue);
-    console.log(operation);
-    }
-    
 }
+
+function operate(){
+    if (operation == "Add"){
+        Add();
+    }else if (operation == "Subtract"){
+        Subtract();
+    }else if (operation == "Multiply"){
+        Multiply();
+    }else if (operation == "Divide"){
+        Divide();
+    }
+}
+
 function Add(){
     saveValue = parseInt(saveValue) + parseInt(displayValue);
+    displayValue = saveValue;
+    display.textContent = displayValue;
+    console.log("added");
+    console.log("saveValue", saveValue);
 }
 function Subtract(){
     saveValue = saveValue - displayValue;
+    displayValue = saveValue;
+    display.textContent = displayValue;
+    console.log("subtracted");
+    console.log("saveValue", saveValue);
 }
 function Multiply(){
     saveValue = saveValue * displayValue;
+    displayValue = saveValue;
+    display.textContent = displayValue;
+    console.log("multiplied");
+    console.log("saveValue", saveValue);
 }
 function Divide(){
     saveValue = saveValue / displayValue;
+    displayValue = saveValue;
+    display.textContent = displayValue;
+    console.log("divided");
+    console.log("saveValue", saveValue);
 }
 
